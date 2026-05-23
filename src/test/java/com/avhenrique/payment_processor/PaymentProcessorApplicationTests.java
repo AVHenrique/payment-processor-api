91923c494055da3bd02938a6010af24b4ea26681
AVHenrique <ha.vicentini@gmail.com>
2026-05-23 16:29:14 -0300

    chore: initial project setup with Spring Boot

---
+++ src/test/java/com/avhenrique/payment_processor/PaymentProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avhenrique.payment_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
